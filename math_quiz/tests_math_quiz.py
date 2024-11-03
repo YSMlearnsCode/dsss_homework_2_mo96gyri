b1597ea4c729044238e33fc8d730f78467c9cc2e
YSMlearnsCode <yasin.m.syed@fau.de>
2024-11-03 22:06:43 +0100

    Changed unittest

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,40 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import GenerateRandomInteger, ChooseOperator, ProblemAndSolution
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_GenerateRandomInteger(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = GenerateRandomInteger(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
+    def test_ChooseOperator(self):
         # TODO
-        pass
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+        for _ in range(1000):
+             operator = ChooseOperator()
+             self.assertIn(operator, ['+', '-', '*'], "Invalid operator generated")
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+
+    def test_ProblemAndSolution(self):
+        # Test cases for ProblemAndSolution function
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),
+            (10, 3, '-', '10 - 3', 7),
+            (4, 6, '*', '4 * 6', 24),
+            (7, 0, '+', '7 + 0', 7),
+            (9, 1, '-', '9 - 1', 8),
+            (3, 3, '*', '3 * 3', 9),
+        ]
+
+        for num1, num2, operator, expected_problem, expected_answer in test_cases:
+            problem, answer = ProblemAndSolution(num1, num2, operator)
+            self.assertEqual(problem, expected_problem, "Generated problem is incorrect")
+            self.assertEqual(answer, expected_answer, "Calculated answer is incorrect")
 
 if __name__ == "__main__":
     unittest.main()

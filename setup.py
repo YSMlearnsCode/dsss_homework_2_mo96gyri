ad167fec59f7f24e3613cd2d0e6be24a98c51972
YSMlearnsCode <yasin.m.syed@fau.de>
2024-11-03 23:40:18 +0100

    Added setup.py to root and __init__.py to math_quiz

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dsss_homework_2",
+    version="0.1",
+    packages=find_packages(),  # This will find the math_quiz package
+    install_requires=[],
+    description="A package for DSSS Homework 2",
+)
